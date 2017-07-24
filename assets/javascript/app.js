1eed2c6e8b361bbcb3b41cf49f9afde1e8cfbd13
Thomas Murphy <Tom@Thomass-MacBook-Air.local>
2017-07-24 11:09:04 -0400

    format

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,40 +19,40 @@ $(document).on('click', 'button', function (event) {
   $.ajax({
     url: queryURL,
     method: 'GET'
-  }) .done(function (response) {
-    $('img').remove()
-    $('.rating').remove()
-    var results = response.data
+  })
 
-    for (var i = 0; i < topics.length; i++) {
-      var gifDiv = $('<div class="gif">')
-      var rating = results[i].rating
-      var displayrating = $('<p>').text('Rating: ' + rating)
-      displayrating.addClass('rating')
+    .done(function (response) {
+      $('img').remove()
+      $('.rating').remove()
+      var results = response.data
+      for (var i = 0; i < topics.length; i++) {
+        var gifDiv = $('<div class="gif">')
+        var rating = results[i].rating
+        var displayrating = $('<p>').text('Rating: ' + rating)
+        displayrating.addClass('rating')
+        var image = $('<img>')
+        image.addClass('imageClass')
+        image.attr('src', results[i].images.fixed_height_still.url)
+        image.attr('data-state', 'still')
+        image.attr('animate', results[i].images.fixed_height_still.url)
+        image.attr('still', results[i].images.fixed_height.url)
+        gifDiv.append(displayrating)
+        gifDiv.append(image)
 
-      var image = $('<img>')
-      image.addClass('imageClass')
-      image.attr('src', results[i].images.fixed_height_still.url)
-      image.attr('data-state', 'still')
-      image.attr('animate', results[i].images.fixed_height_still.url)
-      image.attr('still', results[i].images.fixed_height.url)
-      gifDiv.append(displayrating)
-      gifDiv.append(image)
-
-      $('.gifview').prepend(gifDiv)
-      $('.imageClass').on('click', function () {
-        var state = $(this).attr('data-state')
-        if (state === 'still') {
-          $(this).attr('src', $(this).attr('animate'))
-          $(this).attr('data-state', 'animate')
-        } else {
-          $(this).attr('src', $(this).attr('still'))
-          $(this).attr('data-state', 'still')
-        }
-      })
+        $('.gifview').prepend(gifDiv)
+        $('.imageClass').on('click', function () {
+          var state = $(this).attr('data-state')
+          if (state === 'still') {
+            $(this).attr('src', $(this).attr('animate'))
+            $(this).attr('data-state', 'animate')
+          } else {
+            $(this).attr('src', $(this).attr('still'))
+            $(this).attr('data-state', 'still')
+          }
+        })
+      }
     }
-  }
-  )
+    )
 })
 $('#addbutton').on('click', function (event) {
   event.preventDefault()
